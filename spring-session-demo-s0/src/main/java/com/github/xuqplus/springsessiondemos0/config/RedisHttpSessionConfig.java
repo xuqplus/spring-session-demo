04778336968dceca9e99c5e834b2374b81a31333
xuqplus <445172495@qq.com>
2019-06-28 18:41:07 +0800

    @EnableRedisHttpSession

---
+++ spring-session-demo-s0/src/main/java/com/github/xuqplus/springsessiondemos0/config/RedisHttpSessionConfig.java
@@ -0,0 +1,8 @@
+package com.github.xuqplus.springsessiondemos0.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisHttpSessionConfig {}
---
+++ spring-session-demo-s1/src/main/java/com/github/xuqplus/springsessiondemos1/config/RedisHttpSessionConfig.java
@@ -0,0 +1,8 @@
+package com.github.xuqplus.springsessiondemos1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisHttpSessionConfig {}
