20f59a4114c2cda6a023226392dc01e7fce64c84
xjplus <xxjplus@github.com>
2019-06-28 23:51:04 +0800

    session共享,cookie在浏览器实现里并不区分端口号

--- spring-session-demo-s0/src/main/java/com/github/xuqplus/springsessiondemos0/controller/AController.java
+++ spring-session-demo-s0/src/main/java/com/github/xuqplus/springsessiondemos0/controller/AController.java
@@ -1,12 +1,27 @@
 package com.github.xuqplus.springsessiondemos0.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class AController {
+  Logger logger = LoggerFactory.getLogger(AController.class);
+
   @GetMapping("")
-  public String a() {
+  public String a(HttpSession session) {
+    logger.info("session.getId()=>{}", session.getId());
     return "ok, s0";
+  }
+
+  @GetMapping("setSession")
+  public String setSession(HttpSession session, String k, String v) {
+    logger.info("session.getId()=>{}", session.getId());
+    logger.info("setSession, k=>{}, v=>{}", k, v);
+    session.setAttribute(k, v);
+    return "ok, s0, setSession";
   }
 }
--- spring-session-demo-s1/src/main/java/com/github/xuqplus/springsessiondemos1/controller/AController.java
+++ spring-session-demo-s1/src/main/java/com/github/xuqplus/springsessiondemos1/controller/AController.java
@@ -1,12 +1,27 @@
 package com.github.xuqplus.springsessiondemos1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class AController {
+  Logger logger = LoggerFactory.getLogger(AController.class);
+
   @GetMapping("")
-  public String a() {
+  public String a(HttpSession session) {
+    logger.info("session.getId()=>{}", session.getId());
     return "ok, s1";
+  }
+
+  @GetMapping("getSession")
+  public String getSession(HttpSession session, String k) {
+    logger.info("session.getId()=>{}", session.getId());
+    Object v = session.getAttribute(k);
+    logger.info("getSession, k=>{}, v=>{}", k, v);
+    return String.format("ok, s1, getSession, k=%s, v=%s", k, v);
   }
 }
