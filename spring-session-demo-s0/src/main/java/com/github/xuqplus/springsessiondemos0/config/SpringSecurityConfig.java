b1a014789193bf34152986ede7b8d74eddc9e244
xjplus <xxjplus@github.com>
2019-06-29 14:28:26 +0800

    方法@Autowired

---
+++ spring-session-demo-s0/src/main/java/com/github/xuqplus/springsessiondemos0/config/SpringSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.github.xuqplus.springsessiondemos0.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
--- spring-session-demo-s0/src/test/java/com/github/xuqplus/springsessiondemos0/SpringSessionDemoS0ApplicationTests.java
+++ spring-session-demo-s0/src/test/java/com/github/xuqplus/springsessiondemos0/SpringSessionDemoS0ApplicationTests.java
@@ -2,13 +2,24 @@ package com.github.xuqplus.springsessiondemos0;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringSessionDemoS0ApplicationTests {
 
+  protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired
+  ApplicationContext context;
+
   @Test
-  public void contextLoads() {}
+  public void contextLoads() {
+    logger.info("");
+  }
 }
